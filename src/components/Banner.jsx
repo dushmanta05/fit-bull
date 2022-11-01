a6ea0e22577432acfd75e78e1a62b07de87b2d3f
Diilu Behera <imdilub@gmail.com>
2022-11-02 02:05:49 +0530

    first commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+
+const Banner = () => {
+  return (
+    <Box>
+        <Typography color='#FF2625' fontWeight='600' fontSize='26px'>
+            FitBull
+        </Typography>
+    </Box>
+  )
+}
+
+export default Banner
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import { Box } from '@mui/material'
+import Banner from '../components/Banner'
+import SearchExercises from '../components/SearchExercises'
+import Exercises from '../components/Exercises'
+
+const Home = () => {
+  return (
+    <Box>
+      <Banner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  )
+}
+
+export default Home
