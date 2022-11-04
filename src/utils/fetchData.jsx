e4bcf6f7469712d7e4075f96786a8b9ead1d8221
Diilu Behera <imdilub@gmail.com>
2022-11-04 23:50:46 +0530

    Vite Key update

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -1,14 +1,12 @@
-import { StayPrimaryPortrait } from "@mui/icons-material";
-
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY
+      'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com/exercises/bodyPartList'
     }
   };
 
-export async function fetchData(url, options) {
+export const fetchData= async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
 
