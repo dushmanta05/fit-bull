e1e133987528c9688db58abc3dd65c2f99545207
Diilu Behera <imdilub@gmail.com>
2022-11-20 22:28:20 +0530

    updating handleSearch

--- src/components/HorizontalScrollBar.jsx
+++ src/components/HorizontalScrollBar.jsx
@@ -10,8 +10,8 @@ const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
 
   return (
-    <Typography onClick={() => scrollPrev()} className="right-arrow">
-      <img src={LeftArrowIcon} alt="right-arrow" />
+    <Typography onClick={() => scrollPrev()} className="left-arrow">
+      <img src={LeftArrowIcon} alt="left-arrow" />
     </Typography>
   );
 };
@@ -20,7 +20,7 @@ const RightArrow = () => {
   const { scrollNext } = useContext(VisibilityContext);
 
   return (
-    <Typography onClick={() => scrollNext()} className="left-arrow">
+    <Typography onClick={() => scrollNext()} className="right-arrow">
       <img src={RightArrowIcon} alt="right-arrow" />
     </Typography>
   );
@@ -28,7 +28,7 @@ const RightArrow = () => {
 
 const HorizontalScrollBar = ( {data, bodyPart, setBodyPart} ) => {
   return (
-    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} >{data.map((item) => (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>{data.map((item) => (
         <Box key={item.id || item}
         itemID={item.id || item}
         title={item.id || item}
