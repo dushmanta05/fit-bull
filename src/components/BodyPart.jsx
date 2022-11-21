3b58709629eb0871c09bb5d47c0f6304d84acb52
Diilu Behera <imdilub@gmail.com>
2022-11-21 22:42:13 +0530

    onClick function changed

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Stack, Typography } from '@mui/material';
 import Icon from '../assets/icons/gym.png';
 
-const BodyPart = ({ item, setBodyPart, bodyPart }) => (
+const BodyPart = ({ item, setBodyPart, bodyPart }) => { 
+  return (
   <Stack
     type="button"
     alignItems="center"
@@ -25,6 +26,6 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
     <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
     <Typography fontSize='24px' fontWeight='bold' color='#3a1212' textTransform='capitalize' >{item}</Typography>
   </Stack>
-);
+)};
 
 export default BodyPart;
