e88531bd4e8d26271eb550169e903a2627edeaea
Diilu Behera <imdilub@gmail.com>
2022-11-21 22:53:20 +0530

    targetMuscle & equipment data fetched

--- src/pages/Exercise.jsx
+++ src/pages/Exercise.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { Box } from "@mui/material"
+
+import Detail from "../components/Detail"
+import SimilarExercises from "../components/SimilarExercises"
+
+import { exerciseOptions, fetchData } from '../utils/fetchData'
 
 const Exercise = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+  const [equipmentExercises, setEquipmentExercises] = useState([]);
+
+  const {id} = useParams();
+
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData)
+
+      const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+      setTargetMuscleExercises(targetMuscleExercisesData);
+
+      const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+      setEquipmentExercises(equipmentExercisesData);
+      
+    }
+    fetchExerciseData();
+  }, [id])
+
   return (
-    <div></div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <SimilarExercises 
+      targetMuscleExercises={targetMuscleExercises} 
+      equipmentExercises={equipmentExercises} />
+    </Box>
   )
 }
 
