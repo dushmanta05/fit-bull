f4cd60fef1a7ffb955e0528cedc1fbba40c0cad5
Diilu Behera <imdilub@gmail.com>
2022-11-22 10:38:10 +0530

    updating Box color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import Logo from '../assets/images/Logo.png'
 
 const Footer = () => {
   return (
-    <Box mt='80px' bgcolor='#fff3f4'>
+    <Box mt='80px' bgcolor='#FFFafb'>
       <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
         <img src={Logo} alt='logo' width='48px' height='48px'/>
         <Typography variant='h5' pb='40px' mt= '20px'>
