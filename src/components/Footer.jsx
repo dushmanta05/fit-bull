eb5bad23b369a380eb1c6386c3c02312e325fee8
Diilu Behera <dushmanta0511@gmail.com>
2023-01-07 11:05:57 +0530

    updating footer emoji

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
         <img src={Logo} alt='logo' width='48px' height='48px'/>
         <Typography variant='h5' pb='40px' mt= '20px'>
-          made with ❤️ by Dilu
+          made with 💜 by Dushmanta
         </Typography>
       </Stack>
     </Box>
